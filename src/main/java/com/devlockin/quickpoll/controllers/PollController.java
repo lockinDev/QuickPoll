8f26c7d1959fb109f9da4ccb0306297fc91080d4
Javier <javieralmario12@gmail.com>
2021-05-31 03:47:54 -0400

    Added first method get in PollController, retrieve all the polls.

--- src/main/java/com/devlockin/quickpoll/controllers/PollController.java
+++ src/main/java/com/devlockin/quickpoll/controllers/PollController.java
@@ -2,9 +2,12 @@ package com.devlockin.quickpoll.controllers;
 
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.devlockin.quickpoll.entities.Poll;
 import com.devlockin.quickpoll.repositories.PollRepository;
 
 @RestController
@@ -12,9 +15,17 @@ public class PollController {
 	
 	/*
 	 * Is added Inject unless Autowired  
-	 * to be more compliant
+	 * to be more compliant.
 	 * */
 	@Inject
 	private PollRepository pollRepository;
+	
+	
+	@GetMapping("/polls")
+	public ResponseEntity<Iterable<Poll>> getAllPolls(){
+		Iterable<Poll> allPolls = pollRepository.findAll();
+		return new ResponseEntity<>(allPolls, HttpStatus.OK);
+	}
+	
 	
 }
