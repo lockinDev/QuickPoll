2f8c37e379025c60d9111123e9b1ea2f00fe8c11
Javier <javieralmario12@gmail.com>
2021-06-03 05:01:38 -0400

    Set the location header for the newly created poll resource.

--- src/main/java/com/devlockin/quickpoll/controllers/PollController.java
+++ src/main/java/com/devlockin/quickpoll/controllers/PollController.java
@@ -1,13 +1,17 @@
 package com.devlockin.quickpoll.controllers;
 
+import java.net.URI;
+
 import javax.inject.Inject;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devlockin.quickpoll.entities.Poll;
 import com.devlockin.quickpoll.repositories.PollRepository;
@@ -32,8 +36,16 @@ public class PollController {
 	
 	@PostMapping("/polls")
 	public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
-	        poll = pollRepository.save(poll);
-	        return new ResponseEntity<>(null, HttpStatus.CREATED);
+		poll = pollRepository.save(poll);
+      
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPollUri = ServletUriComponentsBuilder
+                                .fromCurrentRequest()
+                                .path("/{id}")
+                                .buildAndExpand(poll.getId())
+                                .toUri();
+        responseHeaders.setLocation(newPollUri);
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
 	}
 	
 	
