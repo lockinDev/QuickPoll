215bc612e65e01ae71c47827b7aa97e17bd1c6c8
Javier <javieralmario12@gmail.com>
2021-06-04 05:10:10 -0400

    Added method to retrieve a specific poll by id.

--- src/main/java/com/devlockin/quickpoll/controllers/PollController.java
+++ src/main/java/com/devlockin/quickpoll/controllers/PollController.java
@@ -1,6 +1,7 @@
 package com.devlockin.quickpoll.controllers;
 
 import java.net.URI;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
@@ -8,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,11 +35,18 @@ public class PollController {
 		return new ResponseEntity<>(allPolls, HttpStatus.OK);
 	}
 	
+	@GetMapping("/polls/{pollId}")
+	public ResponseEntity<?> getPoll(@PathVariable Long pollId) throws Exception {
+	        Optional<Poll> poll = pollRepository.findById(pollId);
+	        if(!poll.isPresent()) {
+	                throw new Exception("Pool not found");
+	        }
+	        return new ResponseEntity<>(poll.get(), HttpStatus.OK);
+	}
 	
 	@PostMapping("/polls")
 	public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
 		poll = pollRepository.save(poll);
-      
         HttpHeaders responseHeaders = new HttpHeaders();
         URI newPollUri = ServletUriComponentsBuilder
                                 .fromCurrentRequest()
