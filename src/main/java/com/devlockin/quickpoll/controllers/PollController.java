b3a5bf55f2b95869f7d845a85e159bc036736815
Javier <javieralmario12@gmail.com>
2021-06-06 17:32:10 -0400

    Added method to delete a specific poll.

--- src/main/java/com/devlockin/quickpoll/controllers/PollController.java
+++ src/main/java/com/devlockin/quickpoll/controllers/PollController.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,11 @@ public class PollController {
 	        return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
+	@DeleteMapping("/polls/{pollId}")
+	public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+	        pollRepository.deleteById(pollId);
+	        return new ResponseEntity<>(HttpStatus.OK);
+	}
 	
 	
 }
