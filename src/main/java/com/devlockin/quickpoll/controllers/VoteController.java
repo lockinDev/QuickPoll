e0244e4c84d368bd6b47816d67d8f0db94af1a99
Javier <javieralmario12@gmail.com>
2021-06-08 10:39:27 -0400

    Added method to post to create a vote.

--- src/main/java/com/devlockin/quickpoll/controllers/VoteController.java
+++ src/main/java/com/devlockin/quickpoll/controllers/VoteController.java
@@ -2,14 +2,31 @@ package com.devlockin.quickpoll.controllers;
 
 import javax.inject.Inject;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devlockin.quickpoll.entities.Vote;
 import com.devlockin.quickpoll.repositories.VoteRepository;
 
 @RestController
 public class VoteController {
-	
-	 @Inject
-     private VoteRepository voteRepository;
+
+	@Inject
+	private VoteRepository voteRepository;
+
+	@PostMapping("/polls/{pollId}/votes")
+	public ResponseEntity<?> createVote(@PathVariable Long pollId, @RequestBody Vote vote) {
+		vote = voteRepository.save(vote);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.setLocation(
+				ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(vote.getId()).toUri());
+		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+	}
 
 }
