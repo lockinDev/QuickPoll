f3a6064ade716296bd261c5fda9a6c663afae97f
Javier <javieralmario12@gmail.com>
2021-06-11 05:46:46 -0400

    Added helper package and the dto class OptionCount.

---
+++ src/main/java/com/devlockin/quickpoll/helpers/dto/OptionCount.java
@@ -0,0 +1,23 @@
+package com.devlockin.quickpoll.helpers.dto;
+
+public class OptionCount {
+    private Long optionId;
+    private int count;
+    
+	public OptionCount(Long optionId, int count) {
+		super();
+		this.optionId = optionId;
+		this.count = count;
+	}
+	
+	public Long getOptionId() {
+		return optionId;
+	}
+	public int getCount() {
+		return count;
+	}
+    
+    
+    
+
+}
