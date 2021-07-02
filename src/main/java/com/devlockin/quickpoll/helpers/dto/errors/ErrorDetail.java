1dda7c46b4ebd7a5dd71000b4c595b9f31fffb5c
Javier <javieralmario12@gmail.com>
2021-07-01 23:32:51 -0400

    Added reference validationError to ErrorDetail

--- src/main/java/com/devlockin/quickpoll/helpers/dto/errors/ErrorDetail.java
+++ src/main/java/com/devlockin/quickpoll/helpers/dto/errors/ErrorDetail.java
@@ -1,18 +1,27 @@
 package com.devlockin.quickpoll.helpers.dto.errors;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ErrorDetail {
 	
 	private String title, description, developerMessage;
 	private int status;
 	private long timeStamp;
+	private Map<String, List<ValidationError>> errors = new HashMap()();
 	
-	public ErrorDetail(String title, String description, String developerMessage, int status, long timeStamp) {
+	
+
+	public ErrorDetail(String title, String description, String developerMessage, int status, long timeStamp,
+			Map<String, List<ValidationError>> errors) {
 		super();
 		this.title = title;
 		this.description = description;
 		this.developerMessage = developerMessage;
 		this.status = status;
 		this.timeStamp = timeStamp;
+		this.errors = errors;
 	}
 
 	public String getTitle() {
