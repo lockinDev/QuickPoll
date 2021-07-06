5a28ce869ed742d7e2da62f23c92ca8b87f0832a
Javier <javieralmario12@gmail.com>
2021-07-06 00:14:01 -0400

    Added method handleHttpMessageNotReadable to manage the HttpMessageNotReadableException with ErrorDetail format.

--- src/main/java/com/devlockin/quickpoll/handler/RestExceptionHandler.java
+++ src/main/java/com/devlockin/quickpoll/handler/RestExceptionHandler.java
@@ -10,23 +10,27 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.devlockin.quickpoll.exceptions.ResourceNotFoundException;
 import com.devlockin.quickpoll.helpers.dto.errors.ErrorDetail;
 import com.devlockin.quickpoll.helpers.dto.errors.ValidationError;
 
 @ControllerAdvice
-public class RestExceptionHandler {
-	
-	 @Inject
-     private MessageSource messageSource;
-	
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Inject
+	private MessageSource messageSource;
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe,
 			HttpServletRequest request) {
@@ -37,7 +41,7 @@ public class RestExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<?> handleValidationError(MethodArgumentNotValidException manve, HttpServletRequest request) {
-		
+
 		Map<String, List<ValidationError>> errors = new HashMap();
 		List<FieldError> fieldErrors = manve.getBindingResult().getFieldErrors();
 		for (FieldError fe : fieldErrors) {
@@ -50,11 +54,21 @@ public class RestExceptionHandler {
 			ValidationError validationError = new ValidationError(fe.getCode(), messageSource.getMessage(fe, null));
 			validationErrorList.add(validationError);
 		}
-		
+
 		ErrorDetail errorDetail = new ErrorDetail("Validation Failed", manve.getMessage(), manve.getClass().getName(),
 				HttpStatus.BAD_REQUEST.value(), new Date().getTime(), errors);
-		
+
 		return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ErrorDetail errorDetail = new ErrorDetail("Message Not Readable", ex.getMessage(), ex.getClass().getName(),
+				status.value(), new Date().getTime());
+
+		return handleExceptionInternal(ex, errorDetail, headers, status, request);
 	}
 
 }
