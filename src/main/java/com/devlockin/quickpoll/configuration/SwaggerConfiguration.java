65a80d57df68729fee1b36c4514f21de91a47c7f
Javier <javieralmario12@gmail.com>
2021-07-11 23:51:32 -0400

    Added method with ApiInfo on SwaggerConfiguration class.

--- src/main/java/com/devlockin/quickpoll/configuration/SwaggerConfiguration.java
+++ src/main/java/com/devlockin/quickpoll/configuration/SwaggerConfiguration.java
@@ -1,5 +1,7 @@
 package com.devlockin.quickpoll.configuration;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.
@@ -7,6 +9,8 @@ ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,6 +21,17 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "QuickPoll REST API",
+                "QuickPoll Api for creating and managing polls",
+                "http://owndomain.com/terms-of-service",
+                "Terms of service",
+                new Contact("LockinDev", "www.example.com", "info@example.com"),
+                "MIT License", "http://opensource.org/licenses/MIT",Collections.emptyList());
     }
 }
