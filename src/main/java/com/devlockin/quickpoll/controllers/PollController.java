49931dc3c47e928437070fb805b2eb0e35c249eb
Javier <javieralmario12@gmail.com>
2021-07-14 00:17:35 -0400

    Added ApiResponse to poll controller.

--- src/main/java/com/devlockin/quickpoll/controllers/PollController.java
+++ src/main/java/com/devlockin/quickpoll/controllers/PollController.java
@@ -20,10 +20,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devlockin.quickpoll.entities.Poll;
 import com.devlockin.quickpoll.exceptions.ResourceNotFoundException;
+import com.devlockin.quickpoll.helpers.dto.errors.ErrorDetail;
 import com.devlockin.quickpoll.repositories.PollRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @Api(value = "polls", description = "Poll API")
@@ -35,20 +38,22 @@ public class PollController {
 	@Inject
 	private PollRepository pollRepository;
 
-	@ApiOperation(value = "Retrieves all the polls", response=Poll.class, responseContainer="List")
+	@ApiOperation(value = "Retrieves all the polls", response = Poll.class, responseContainer = "List")
 	@GetMapping("/polls")
 	public ResponseEntity<Iterable<Poll>> getAllPolls() {
 		Iterable<Poll> allPolls = pollRepository.findAll();
 		return new ResponseEntity<>(allPolls, HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "Retrieves a Poll associated with the pollId", response=Poll.class)
+	@ApiOperation(value = "Retrieves a Poll associated with the pollId", response = Poll.class)
 	@GetMapping("/polls/{pollId}")
 	public ResponseEntity<?> getPoll(@PathVariable Long pollId) throws Exception {
 		return new ResponseEntity<>(verifyPoll(pollId), HttpStatus.OK);
 	}
-	
-	@ApiOperation(value = "Creates a new Poll", notes="The newly created poll Id will be sent in the location response header", response = Void.class)
+
+	@ApiOperation(value = "Creates a new Poll", notes = "The newly created poll Id will be sent in the location response header", response = Void.class)
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Poll Created Successfully", response = Void.class),
+			@ApiResponse(code = 500, message = "Error creating Poll", response = ErrorDetail.class) })
 	@PostMapping("/polls")
 	public ResponseEntity<?> createPoll(@Valid @RequestBody Poll poll) {
 		poll = pollRepository.save(poll);
@@ -61,16 +66,16 @@ public class PollController {
 
 	@PutMapping("/polls/{pollId}")
 	public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
-		 	verifyPoll(pollId);
-	        pollRepository.save(poll);
-	        return new ResponseEntity<>(HttpStatus.OK);
+		verifyPoll(pollId);
+		pollRepository.save(poll);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@DeleteMapping("/polls/{pollId}")
 	public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
 		verifyPoll(pollId);
 		pollRepository.deleteById(pollId);
-        return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	private Poll verifyPoll(Long pollId) throws ResourceNotFoundException {
