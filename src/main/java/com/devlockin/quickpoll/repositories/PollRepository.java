0e576362fd41eb9d60e9bd3a2e343af4ea97d23b
Javier <javieralmario12@gmail.com>
2021-07-20 23:51:29 -0400

    Modified the poll repository to add paging.

--- src/main/java/com/devlockin/quickpoll/repositories/PollRepository.java
+++ src/main/java/com/devlockin/quickpoll/repositories/PollRepository.java
@@ -1,8 +1,8 @@
 package com.devlockin.quickpoll.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.devlockin.quickpoll.entities.Poll;
 
-public interface PollRepository extends CrudRepository<Poll, Long> {
+public interface PollRepository extends PagingAndSortingRepository<Poll, Long> {
 }
