f8b8921bfc3b917e7b8e5bf920b74bf106060116
Javier <javieralmario12@gmail.com>
2021-08-05 06:30:47 -0400

    Added configuration to allow access to v1 and v2.

--- src/main/java/com/devlockin/quickpoll/configuration/SecurityConfig.java
+++ src/main/java/com/devlockin/quickpoll/configuration/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.devlockin.quickpoll.configuration;
 import javax.inject.Inject;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -17,6 +19,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	        http
+	          .sessionManagement()
+	             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	             .and()
+	          .authorizeRequests()
+	             .antMatchers("/v1/**", "/v2/**", "/swagger-ui/**", "/api-­docs/**").permitAll()
+	             .antMatchers("/v3/polls/ **").authenticated()
+	             .and()
+	          .httpBasic()
+	             .realmName("Quick Poll")
+	             .and()
+	          .csrf()
+	             .disable();
 	}
 
 }
