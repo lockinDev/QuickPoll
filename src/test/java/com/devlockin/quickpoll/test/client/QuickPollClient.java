10d094b3e3e842b43e298eb8d27091fd33ae821b
Javier <javieralmario12@gmail.com>
2021-08-08 21:51:22 -0400

    Added test module, first element the client using restTemplate.

---
+++ src/test/java/com/devlockin/quickpoll/test/client/QuickPollClient.java
@@ -0,0 +1,16 @@
+package com.devlockin.quickpoll.test.client;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.devlockin.quickpoll.entities.Poll;
+
+public class QuickPollClient {
+
+	private static final String QUICK_POLL_URI_V1 = "http://localhost:8080/v1/polls";
+	private RestTemplate restTemplate = new RestTemplate();
+
+	public Poll getPollById(Long pollId) {
+		return restTemplate.getForObject(QUICK_POLL_URI_V1 + "/{pollId}", Poll.class, pollId);
+	}
+
+}
