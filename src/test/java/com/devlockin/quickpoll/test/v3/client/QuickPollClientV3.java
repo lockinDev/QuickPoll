c4295ee172fd605a92193ecdefa16e259ba911ce
Javier <javieralmario12@gmail.com>
2021-08-17 12:04:51 -0400

    Added method that include autorization in the header for the version 3 of the client.

--- src/test/java/com/devlockin/quickpoll/test/v3/client/QuickPollClientV3.java
+++ src/test/java/com/devlockin/quickpoll/test/v3/client/QuickPollClientV3.java
@@ -1,11 +1,12 @@
 package com.devlockin.quickpoll.test.v3.client;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
 
 import com.devlockin.quickpoll.entities.Poll;
 
@@ -14,14 +15,13 @@ public class QuickPollClientV3 {
 	private static final String QUICK_POLL_URI_3 = "http://localhost:8080/v3/polls";
 	private RestTemplate restTemplate = new RestTemplate();
 
-	public PageWrapper<Poll> getAllPolls(int page, int size) {
-		ParameterizedTypeReference<PageWrapper<Poll>> responseType = new ParameterizedTypeReference<PageWrapper<Poll>>() {
-		};
-		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(QUICK_POLL_URI_3).queryParam("page", page)
-				.queryParam("size", size);
-		ResponseEntity<PageWrapper<Poll>> responseEntity = restTemplate.exchange(builder.build().toUri(),
-				HttpMethod.GET, null, responseType);
-		return responseEntity.getBody();
+
+	private HttpHeaders getAuthenticationHeader(String username, String password) {
+		String credentials = username + ":" + password;
+		byte[] base64CredentialData = Base64.encodeBase64(credentials.getBytes());
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Basic " + new String(base64CredentialData));
+		return headers;
 	}
 
 }
