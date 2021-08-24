17697b785fd8fb4dbfc52073804431122c9568ec
Javier <javieralmario12@gmail.com>
2021-08-23 23:04:59 -0400

    Added integration test to the poll entities.

---
+++ src/test/java/com/devlockin/quickpoll/test/client/controllers/PollControllerIT.java
@@ -0,0 +1,41 @@
+package com.devlockin.quickpoll.test.client.controllers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.devlockin.quickpoll.test.client.QuickPollTestApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = QuickPollTestApplication.class)
+@WebAppConfiguration
+public class PollControllerIT {
+
+	@Inject
+	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testGetAllPolls() throws Exception {
+		mockMvc.perform(get("/v1/polls")).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(20)));
+	}
+
+}
